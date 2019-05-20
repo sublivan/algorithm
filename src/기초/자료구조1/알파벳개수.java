14a5431fe1cee981fc269bbbf0ee477d62846b14
meeansub <welcltm@gmail.com>
2019-05-20 21:44:23 +0900

    10808 알파벳 개수

---
+++ src/기초/자료구조1/알파벳개수.java
@@ -0,0 +1,35 @@
+package 기초.자료구조1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class 알파벳개수 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        HashMap<Character,Integer> alpabet =new HashMap<>();
+        int i;
+        for(i=0;i<26;i++){
+            alpabet.put((char)('a'+i),0);
+        }
+
+        int n=str.length();
+        int count=0;
+        for(i=0;i<n;i++){
+            char compare=str.charAt(i);
+            if(alpabet.containsKey(compare)){
+                count=alpabet.get(compare)+1;
+                alpabet.put(compare,count);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char c:alpabet.keySet()){
+            int result=alpabet.get(c);
+            sb.append(result+" ");
+        }
+        System.out.println(sb.toString());
+
+
+    }
+}

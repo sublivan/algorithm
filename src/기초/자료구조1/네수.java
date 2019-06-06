38e79f4e9ce94cd72ff0a06ac6d8f71b09504867
meeansub <welcltm@gmail.com>
2019-06-06 23:22:03 +0900

    10824:네수

---
+++ src/기초/자료구조1/네수.java
@@ -0,0 +1,22 @@
+package 기초.자료구조1;
+
+import java.io.*;
+
+
+public class 네수 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] str= br.readLine().split(" ");
+
+        long result=0;
+
+        String ab=str[0]+str[1];
+        String cd=str[2]+str[3];
+
+        result=Long.parseLong(ab)+Long.parseLong(cd);
+
+        wr.write(result+"");
+        wr.close();
+    }
+}

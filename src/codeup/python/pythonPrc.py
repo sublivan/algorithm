78ad2f8c9b2370b731716ae156038cac331601ca
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-03-09 14:11:14 +0900

    update

--- src/codeup/python/pythonPrc.py
+++ src/codeup/python/pythonPrc.py
@@ -2,8 +2,8 @@ import io, sys
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
 
 class Main:
-    x=float(input())
-    print('%.2f' %round(x,2))
+
+
 
 
 
---
+++ src/안경잡이개발자/선택정렬.java
@@ -0,0 +1,28 @@
+package 안경잡이개발자;
+//O(N^2)
+import java.util.Arrays;
+
+public class 선택정렬 {
+    public static void main(String[] args) {
+
+        int temp, min, index = 0;
+        int a[] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+        for (int i = 0; i < 10; i++) {
+            {
+                min = 9999;
+                for (int j = i; j < 10; j++) {
+                    if (min > a[j]) {
+                        min = a[j];
+                        index = j;
+                    }
+                }
+                temp = a[i];
+                a[i] = a[index];
+                a[index] = temp;
+
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}

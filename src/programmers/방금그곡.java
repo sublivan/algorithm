0d3a17a3362a2b086c07091861f762a0f37f37f7
meeansub <welcltm@gmail.com>
2020-08-10 17:30:13 +0900

    2018 kakao blind

--- src/programmers/방금그곡.java
+++ src/programmers/방금그곡.java
@@ -1,45 +1,70 @@
 package programmers;
 
-import java.util.*;
-public class 방금그곡
-{
-
-}
-
-//완성아님
+//2018 KAKAO BLIND RECRUITMENT
 //https://programmers.co.kr/learn/courses/30/lessons/17683
-class Solution {
-    public String solution(String m, String[] musicinfos) {
-        String answer = "";
-        String[] temp;
-        int start;
-        int end;
-        int run;
-        int cnt = 0;
 
-        for(int i =0; i < musicinfos.length; i++)
+import java.util.*;
+
+public class 방금그곡 {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+
+        String[] startArr;
+        String[] endArr;
+
+        String[] temp;
+        String info ="";
+
+        int playHour;
+        int play;
+        String title;
+
+        int maxRun = 0;
+        String code = convertCode(m);
+
+        for(int i = 0; i < musicinfos.length; i++)
         {
             temp = musicinfos[i].split(",");
-            start = Integer.parseInt(temp[0].substring(3));
-            end = Integer.parseInt(temp[1].substring(3));
-            run = end - start;
+            startArr = temp[0].split(":");
+            endArr = temp[1].split(":");
 
-            if(run != temp[3].length())
+            playHour = Integer.parseInt(endArr[0]) - Integer.parseInt(startArr[0]);
+            play = Integer.parseInt(endArr[1]) - Integer.parseInt(startArr[1]) + (playHour * 60);
+            title = temp[2];
+            info = makeMusic(temp[3], play);
+
+            if(info.contains(code) == true)
             {
-                while(temp[3].length() < run)
+                if(maxRun < play)
                 {
-                    temp[3] +=temp[3];
+                    maxRun = play;
+                    answer = title;
                 }
             }
-
-            if(temp[3].contains(m) == true)
-            {
-                answer = temp[2];
-                cnt++;
-            }
         }
-        if(answer.length() == 0)
-            answer = "`(None)`";
         return answer;
+    }
+
+    public String makeMusic(String code, int play)
+    {
+        code = convertCode(code);
+        StringBuilder sb = new StringBuilder();
+        for(int i =0 ; i < play ; i++)
+        {
+            sb.append(code.charAt(i % code.length()));
+        }
+        return sb.toString();
+    }
+
+    public String convertCode(String code)
+    {
+        String[] sharp = {"A#", "C#", "D#", "F#", "G#"};
+        String[] convert = {"a", "c", "d", "f", "g"};
+
+        for(int i = 0; i < sharp.length; i++)
+        {
+            code = code.replaceAll(sharp[i], convert[i]);
+        }
+        return code;
     }
 }

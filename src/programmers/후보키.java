d1d2bcd12a5025622b8383f14c9de33d557b3f3b
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-08-12 21:36:23 +0900

    kakaoblind2019

--- src/programmers/후보키.java
+++ src/programmers/후보키.java
@@ -2,5 +2,12 @@ package programmers;
 
 //https://programmers.co.kr/learn/courses/30/lessons/42890
 //2019 kakao blind
+//bitmask or dfs
 public class 후보키 {
+
+    public int solution(String[][] relation) {
+        int answer = 0;
+
+        return answer;
+    }
 }

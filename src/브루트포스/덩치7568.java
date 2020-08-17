efa468cdbaa73d65b599a7ba5b1f80d330d59d7c
meeansub <welcltm@gmail.com>
2020-08-17 18:57:13 +0900

    update

--- src/브루트포스/덩치7568.java
+++ src/브루트포스/덩치7568.java
@@ -8,22 +8,30 @@ public class 덩치7568 {
         int n = sc.nextInt();
         int x;
         int y;
-        int[] 덩치 = new int[200];
-        Map<Integer, Integer> map = new HashMap<>();
-        while(n-- > 0)
+        int[][] 덩치 = new int[n][3];
+        for(int i =0; i < n; i++)
         {
             x = sc.nextInt();
             y = sc.nextInt();
-
-            map.put(x, y);
+            덩치[i][0] = x;
+            덩치[i][1] = y;
         }
-//        for(int i = 0; i < 200; i++)
-//        {
-//            for(int j = 0; j < 200; j++)
-//            {
-//                System.out.print(덩치[i][j]);
-//            }
-//        }
+        int cnt;
+        for(int i = 0; i < 덩치.length; i++)
+        {
+            cnt = 0;
+            for(int j = 0; j < 덩치.length; j++)
+            {
+                if (덩치[i][0] < 덩치[j][0] && 덩치[i][1] < 덩치[j][1])
+                    cnt++;
+            }
+            덩치[i][2] = cnt + 1;
+        }
+        for(int i = 0; i < 덩치.length; i++)
+        {
+            System.out.print(덩치[i][2] + " ");
+        }
+
 
 
     }

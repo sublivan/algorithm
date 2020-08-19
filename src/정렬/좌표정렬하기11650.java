5b64bed79600f97348d8288f6b0895e3d09cf8b9
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-08-19 21:38:14 +0900

    좌표정렬하기11650

--- src/정렬/좌표정렬하기11650.java
+++ src/정렬/좌표정렬하기11650.java
@@ -1,6 +1,6 @@
 package 정렬;
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class 좌표정렬하기11650 {
     public static void main(String[] args) throws IOException {
@@ -13,26 +13,20 @@ public class 좌표정렬하기11650 {
             map[i][0] = Integer.parseInt(input[0]);
             map[i][1] = Integer.parseInt(input[1]);
         }
-//        for(int i = 1; i < str.length; i++)
-//        {
-//            for(int j = 0; j < str.length - i; j++)
-//            {
-//                int r1 = (int)str[j].charAt(0) + (int)str[j].charAt(2);
-//                int r2 = (int)str[j + 1].charAt(0) + (int)str[j + 1].charAt(2);
-//                if(r1 > r2)
-//                {
-//                    String temp = str[j];
-//                    str[j] = str[j + 1];
-//                    str[j + 1] = temp;
-//                }
-//            }
-//    }
-//        for(String s : str) {
-//            System.out.println(s);
-//        }
+        Arrays.sort(map, new Comparator<int []>() {
+            public int compare(int a[], int b[])
+            {
+                if(a[0] == b[0])
+                    return Integer.compare(a[1], b[1]);
+                else
+                    return Integer.compare(a[0], b[0]);
+            }
+        });
+        for(int i = 0; i < map.length; i++)
+        {
+            System.out.println(map[i][0] + " " + map[i][1]);
+        }
     }
-
-
-
-
 }
+//시간초과주의
+// x 좌표가 같으면 y 좌표를 기준으로 정렬하는게 포인트

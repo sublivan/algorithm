a55f21af2268fc6831aa96880e25a466a4fde312
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-08-31 16:23:04 +0900

    queue로 구현 추가

--- src/programmers/스택_큐/기능개발.java
+++ src/programmers/스택_큐/기능개발.java
@@ -8,6 +8,7 @@ public class 기능개발 {
         int[] speeds = {1, 1, 1, 1, 1, 1};
 
         System.out.println(Arrays.toString(solution(progresses, speeds)));
+//        System.out.println(Arrays.toString(solution2(progresses, speeds)));
     }
 
     public static int[] solution(int[] progresses, int[] speeds) {
@@ -22,6 +23,8 @@ public class 기능개발 {
            }
            else
                days[i] = (100 - progresses[i]) / speeds[i] + 1;
+
+            System.out.println(i + "progress : " + days[i]);
         }
         ArrayList<Integer> result = new ArrayList<>();
 
@@ -35,6 +38,7 @@ public class 기능개발 {
                 if(days[i] < days[j])
                 {
                     i = j - 1;
+                    System.out.println(i);
                     break;
                 }
                 cnt++;
@@ -49,4 +53,47 @@ public class 기능개발 {
         }
         return answer;
     }
+
+//queue를 활용하여 구현
+//    public static int[] solution2(int[] progresses, int[] speeds) {
+//        int[] answer= {};
+//        Queue<Integer> que = new LinkedList<>();
+//        for(int i = 0; i < progresses.length; i++)
+//        {
+//            int day = (100 - progresses[i]) % speeds[i] == 0 ?
+//                    (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
+//            que.add(day);
+//        }
+//
+//        System.out.println(Arrays.toString(que.toArray()));
+//
+//        ArrayList<Integer> list = new ArrayList<>();
+//        int cnt = 1;
+//        int prev = que.poll();
+//        while(!que.isEmpty())
+//        {
+//            int current = que.poll();
+//            System.out.println("prev : " + prev + " curr : " + current );
+                //첫번째 기능 개발 기간이 다음 개발 기간보다 크거나 같다면
+                //첫번째 기능을 배포할때 다음 기능도 같이 배포해야된다
+//            if(prev >= current)
+//                cnt++;
+//            else // 그렇지 않다면 이전 기능만 배포된다
+//            {
+//                System.out.println("cnt : " + cnt);
+//                prev = current;
+//                list.add(cnt);
+//                cnt = 1;
+//            }
+//        }
+//        list.add(cnt);
+//
+//        answer = new int[list.size()];
+//        for(int i = 0 ; i < list.size(); i++)
+//        {
+//            answer[i] = list.get(i);
+//        }
+//
+//        return answer;
+//    }
 }

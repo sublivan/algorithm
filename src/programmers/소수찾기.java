7b6afc58af4780d022dafa437a7ab420e553bafa
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-09-08 21:49:54 +0900

    update

--- src/programmers/소수찾기.java
+++ src/programmers/소수찾기.java
@@ -5,16 +5,17 @@ import java.util.*;
 public class 소수찾기 {
     public static void main(String[] args)
     {
-        int n = 5;
+        int n = 10;
         System.out.println(solution(n));
     }
+
     public static int solution(int n) {
         int answer = 0;
         boolean isPrime;
         for(int i = 2; i <= n; i++)
         {
             isPrime = true;
-            for(int j = 2 ; j * j <= i; j++) {
+            for(int j = 2 ; j * j<= i; j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
@@ -49,4 +50,6 @@ public class 소수찾기 {
 }
 /*
 어떤수의 약수는 약수를 제곱했을때 어떤수보다 클 수 없다.
+
+약수는 자기자신 / 2 보다 클수 없다
  */

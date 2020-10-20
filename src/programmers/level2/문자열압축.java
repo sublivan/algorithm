f89c4ab60ce3c912445d6dbb9748c4fdce60959d
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-10-21 00:17:04 +0900

    x

---
+++ src/programmers/level2/문자열압축.java
@@ -0,0 +1,9 @@
+package programmers.level2;
+
+public class 문자열압축 {
+    public int solution(String s) {
+
+        int answer = s.length();
+        return answer;
+    }
+}
--- src/programmers/완전탐색/소수찾기_x.java
+++ src/programmers/완전탐색/소수찾기_x.java
@@ -4,35 +4,9 @@ package programmers.완전탐색;
 import java.util.*;
 
 public class 소수찾기_x {
-    public static void main(String[] args)
-    {
-        String numbers = "17";
-        System.out.println(solution(numbers));
-    }
 
-    static List<Integer> list = new ArrayList<>();
-
-    public static int solution(String numbers) {
+    public int solution(String numbers) {
         int answer = 0;
-
-        String[] nums = numbers.split("");
-        for(int value : list)
-        {
-            if(value > 2)
-            {
-                answer++;
-            }
-        }
         return answer;
-    }
-    public static boolean prime(int num)
-    {
-        if(num <= 1)
-            return false;
-        else if(num == 2 || num == 3)
-            return true;
-        else if(num % 2 == 0 || num % 3 == 0)
-            return false;
-        return true;
     }
 }

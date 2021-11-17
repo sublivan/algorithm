582a2a0bb47805ef038bc46954ee0c654e2e2ec0
김민섭 <33247303+meeansub@users.noreply.github.com>
2021-11-18 00:20:55 +0900

    숫자문자열과영단어

--- src/programmers/level1/숫자문자열과영단어.java
+++ src/programmers/level1/숫자문자열과영단어.java
@@ -1,4 +1,32 @@
 package programmers.level1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class 숫자문자열과영단어 {
+
+    public int solution(String s) {
+        Map<String, Integer> convertMap = new HashMap<>() {
+            {
+                put("zero", 0);
+                put("one", 1);
+                put("two", 2);
+                put("three", 3);
+                put("four", 4);
+                put("five", 5);
+                put("six", 6);
+                put("seven", 7);
+                put("eight", 8);
+                put("nine", 9);
+            }
+        };
+
+        for (String number : convertMap.keySet()) {
+            if (s.contains(number)) {
+                s = s.replaceAll(number, String.valueOf(convertMap.get(number)));
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
 }

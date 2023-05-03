3cd32d95e1127b79a330a5f7a17ea7995ec45dbb
MINSUB KIM <sublivan29@gmail.com>
2023-05-03 16:25:15 +0900

    [배열과리스트] 구간합구하기2 004

--- src/doit/구간합구하기2_004.java
+++ src/doit/구간합구하기2_004.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * BOJ 11660
+ */
 public class 구간합구하기2_004 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
